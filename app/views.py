3db609a1ce7faf0d4116e6c7a30a9f0386378d30
quaintm <mquaintance@gmail.com>
2014-11-19 18:01:43 -0500

    added login view

--- app/views.py
+++ app/views.py
@@ -15,5 +15,10 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash(('Login requested for UserID=%s, Password=%s, remember_me=%s') 
+      % (str(form.userid.data), str(form.password.data), 
+        str(form.remember_me.data)))
+    return redirect('/index')
   return render_template('login.html',
     title = 'Sign In', form=form)
