7af7fe99579d3b869c012afddc9c5b16d4717e94
quaintm <mquaintance@gmail.com>
2014-11-19 16:46:04 -0500

    first pass at flask app using -security

---
+++ app/views.py
@@ -0,0 +1,12 @@
+# first views
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+  user = {'nickname': 'Will'} # fake user
+  
+  return render_template('index.html',
+    title='Home', user=user)
