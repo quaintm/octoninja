b49951671cfb117f25635ef085f008399cc04bed
quaintm <mquaintance@gmail.com>
2014-11-19 17:16:12 -0500

    added login page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 # first views
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +11,9 @@ def index():
   
   return render_template('index.html',
     title='Home', user=user)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  return render_template('login.html',
+    title = 'Sign In', form=form)
