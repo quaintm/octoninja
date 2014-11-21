6093139336e86c4cedc2e56550a7596c9352a44f
quaintm <mquaintance@gmail.com>
2014-11-21 15:36:07 -0500

    new login handler using openid

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 # views -- defines each page route for the app
 
-from flask import render_template, flash, redirect
-from app import app
-from .forms import LoginForm
+from flask import render_template, flash, redirect, session, urf_for,
+  request, g
+from flask.ext.login import login_user, logout_user, current_user, 
+  login_required
+from app import app, db, lm, oid
+from forms import LoginForm
+from models import User
 
 @app.route('/')
 @app.route('/index')
@@ -13,12 +17,23 @@ def index():
     title='Home', user=user)
 
 @app.route('/login', methods=['GET','POST'])
+@oid.loginhandler
 def login():
+  if g.user is not None and g.user.is_authenticated():
+    return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    flash(('Login requested for UserID=%s, Password=%s, remember_me=%s') 
-      % (str(form.userid.data), str(form.password.data), 
-        str(form.remember_me.data)))
-    return redirect('/index')
+    session['remember_me'] = form.remember_me.data
+    return oid.try_login(form.openid.data, ask_for=['nickname','email'])
+
+    # flash(('Login requested for UserID=%s, Password=%s, remember_me=%s') 
+    #   % (str(form.userid.data), str(form.password.data), 
+    #     str(form.remember_me.data)))
+    # return redirect('/index')
+
   return render_template('login.html',
     title = 'Sign In', form=form)
+
+@lm.user_loader
+def load_user(id):
+  return User.query.get(int(id))
