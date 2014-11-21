5e0059b148d2e5f768e8c4180683804abdf94dbe
quaintm <mquaintance@gmail.com>
2014-11-21 16:34:35 -0500

    reordered imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from app import views, models
 import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
@@ -9,9 +8,12 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+# app import must happen last, after definitions
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ from app import app, db, lm, oid
 from forms import LoginForm
 from models import User
 
+@lm.user_loader
+def load_user(id):
+  return User.query.get(int(id))
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -15,6 +19,10 @@ def index():
   
   return render_template('index.html',
     title='Home', user=user)
+
+@app.before_request
+def befire_request():
+  g.user = current_user
 
 @app.route('/login', methods=['GET','POST'])
 @oid.loginhandler
@@ -24,7 +32,7 @@ def login():
   form = LoginForm()
   if form.validate_on_submit():
     session['remember_me'] = form.remember_me.data
-    return oid.try_login(form.openid.data, ask_for=['nickname','email'])
+    return oid.try_login(form.userid.data, ask_for=['nickname','email'])
 
     # flash(('Login requested for UserID=%s, Password=%s, remember_me=%s') 
     #   % (str(form.userid.data), str(form.password.data), 
@@ -32,8 +40,28 @@ def login():
     # return redirect('/index')
 
   return render_template('login.html',
-    title = 'Sign In', form=form)
+    title = 'Sign In', form=form,
+    providers = app.config['OPENID_PROVIDERS'])
 
-@lm.user_loader
-def load_user(id):
-  return User.query.get(int(id))
+@oid.after_login
+def after_login(resp):
+  if resp.email is None or resp.email == "":
+    flash('Invalid login.')
+    return redirect(url_for('login'))
+  user = User.query.filter_by(email=resp.email).first()
+  if user is None:
+    nickname = resp.nickname
+    if nickname is None or nickname == "":
+      nickname = resp.email.split('@')[0]
+    user = User(nickname = nickname, email = resp.email)
+    db.session.add(user)
+    db.session.commit()
+  remember_me = False
+  if 'remember_me' in session:
+    remember_me = session['remember_me']
+    session.pop('remember_me',None)
+  login_user(user, remember = remember_me)
+  return rediredct(request.args.get('next') or url_for('index'))
+
+
+
