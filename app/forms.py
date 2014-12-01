e526a508b456911e5af3c6cfeb12321c6298692d
quaintm <mquaintance@gmail.com>
2014-12-01 13:34:58 -0500

    two-space indents

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    userid = StringField('userid', validators=[DataRequired()])
-    # password = StringField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+  userid = StringField('userid', validators=[DataRequired()])
+  # password = StringField('password', validators=[DataRequired()])
+  remember_me = BooleanField('remember_me', default=False)
