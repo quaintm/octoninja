2a96987801850004610e29a7e4d44e60d4a0f73e
quaintm <mquaintance@gmail.com>
2014-12-01 13:21:07 -0500

    added flask-migrate commands

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore, \
-  UserMixin, RoleMixin, login_required
-import os
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from config import basedir
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir
-
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 
 
 db = SQLAlchemy(app)
@@ -17,6 +17,11 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+# commands for flask-migrate
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
 
 # app import must happen last, after definitions
 from app import views, models
