db04c3f819239a10189446de09aaff4b506a1bd9
quaintm <mquaintance@gmail.com>
2014-12-01 14:51:20 -0500

    removed openid auth

--- config.py
+++ config.py
@@ -11,13 +11,3 @@ SECURITY_PASSWORD_SALT = SECRET_KEY
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
-# openid section below:
-
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
