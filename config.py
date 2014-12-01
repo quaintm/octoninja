9eae594d591b4966c79162bf4025a28c1cbb288d
quaintm <mquaintance@gmail.com>
2014-12-01 09:43:22 -0500

    debug flag added

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
