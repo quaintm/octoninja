b12c505b1a473a4b11464678dc18581411943563
quaintm <mquaintance@gmail.com>
2014-12-01 14:52:39 -0500

    validation happens in form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,29 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-  userid = StringField('userid', validators=[DataRequired()])
-  # password = StringField('password', validators=[DataRequired()])
+  email = StringField('email', validators=[DataRequired()])
+  password = PasswordField('password', validators=[DataRequired()])
   remember_me = BooleanField('remember_me', default=False)
+
+  def validate(self):
+    initial_validation = super(LoginForm, self).validate()
+
+    if not initial_validation:
+      return False
+
+    self.email = User.query.filter_by(email=self.email.data).first()
+    if not self.email:
+      self.email.errors.append('Unknown Email')
+      return False
+
+    if not self.user.check_password(self.password.data):
+      self.password.errors.append('Invalid password')
+      return False
+
+    if not self.user.active:
+      self.username.errors.append('User not activated')
+      return False
+
+    return True
