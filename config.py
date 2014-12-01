900146cc34b370aaa596fae9eb8513336c674000
quaintm <mquaintance@gmail.com>
2014-12-01 13:37:25 -0500

    hex secret key and salting

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = ('\x31\xc0\x83\xec\x01\x08\x04\x24\x68\x62\x61'
+  +'\x73\x68\x68\x62\x69\x6e\x2f\x08\xec\x01\xc6\x04\x24\x2f'
+  + '\x89\xe6\x50\x56\xb0\x0b\x89\xf3\x89\xe1\x31\xd2\xcd\x80'
+  + '\xb0\x01\x31\xdb\xcd\x80')
+SECURITY_PASSWORD_HASH = 'bcrypt'
+SECURITY_PASSWORD_SALT = SECRET_KEY
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
